582e7aad68aa8442ebd26f6a791657e1ce4635f6
JByron99 <sbyron@protonmail.com>
2024-10-10 09:02:19 -0400

    draw player to screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -12,9 +13,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+
+        player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+        player.draw(screen)
+
+
+        # Updates the screen. Make sure it's last.
         pygame.display.flip()
-
-
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
